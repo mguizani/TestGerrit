3ebde6e8e4db87d0de118e715f6321302a440a21
mguizani <manel.guizani@tuniteam.com>
2014-01-20 17:10:21 +0100

    nothing

--- TestBranch/src/com/tuniteam/occygen/TestMainClass.java
+++ TestBranch/src/com/tuniteam/occygen/TestMainClass.java
@@ -2,21 +2,7 @@ package com.tuniteam.occygen;
 
 public class TestMainClass {
 	
-String chaine = "line 1 to commit";
-String chaine2 = "line 2 to commit";
-String chaine3 = "line 3 to commit";
-String chaine4 = "line 4 to commit";
-String chaine5 = "line 5 to commit";
-String chaine6 = "line 6 to commit";
-String chaine7 = "line 7 to commit for gerrit";
-
-String chaine11 = "line11 to commit for gerrit";
-String chaine12 = "line12 to commit for gerrit";
-
-String chaine14 = "line14 to commit for gerrit";
-String chaine15 = "line15 to commit for gerrit";
-String chaine16 = "line16 to commit for gerrit";
-String chaine162 = "line162 to commit for gerrit";
+// nothing
 
 
 }
